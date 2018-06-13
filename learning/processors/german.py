78ce72347c7f4b641d14558d5691cb9635d6ce7b
Jayson Reis <santosdosreis@gmail.com>
2018-06-13 20:55:02 +0200

    fix(processors): Fix wrong separable verbs in german

--- learning/processors/german.py
+++ learning/processors/german.py
@@ -16,16 +16,29 @@ class GermanProcessor(BaseProcessor):
             verb_particle = self.get_verb_particle(token)
             if verb_particle:
                 yield f"{verb_particle.text}{token.lemma_}"
-            elif self.is_token_particle(token):
+            elif self.is_token_separable_verb_particle(token):
                 continue  # ignore because someone took care of it
             else:
-                yield token.lemma_
+                yield self.get_token_lemma(token)
 
     def get_verb_particle(self, token: Token) -> Optional[Token]:
         if self.is_token_verb(token):
             particle = [
-                particle for particle in token.children if self.is_token_particle(particle) and particle.head == token
+                particle
+                for particle in token.children
+                if self.is_token_separable_verb_particle(particle) and particle.head == token
             ]
             if particle:
                 return particle[0]
         return None
+
+    def is_token_separable_verb_particle(self, token):
+        return self.is_token_particle(token) and token.tag_ == "PTKVZ"
+
+    def get_token_lemma(self, token):
+        if self.is_token_infititive_verb(token):
+            return token.text
+        return token.lemma_
+
+    def is_token_infititive_verb(self, token):
+        return token.tag_ == "VAINF"
